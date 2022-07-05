426d939b6c58af9d164221aa066079c5d81fa1a3
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2022-07-06 01:04:43 +0400

    .

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from datetime import datetime
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
